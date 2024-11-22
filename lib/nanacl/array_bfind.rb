b3ed431c5ddd4a655d1b11464f7be4129aa93bf1
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-11-23 08:30:01 +0900

    Change: binary_findの仕様を変更

--- lib/nanacl/array_bfind.rb
+++ lib/nanacl/array_bfind.rb
@@ -2,24 +2,24 @@
 
 class Array
   def binary_find(target)
-    range =
-      (
-        if target.is_a?(Range)
-          target
+    if target.is_a?(Range)
+      start = target.begin.nil? ? 0 : bsearch_index { |x| x >= target.begin }
+      finish =
+        if target.end.nil?
+          size
+        elsif target.exclude_end?
+          bsearch_index { |x| x >= target.end }
         else
-          (target..target)
+          bsearch_index { |x| x > target.end }
         end
-      )
-
-    start = bsearch_index { |x| x >= range.begin }
-    finish =
-      if range.exclude_end?
-        bsearch_index { |x| x >= range.end }
-      else
-        bsearch_index { |x| x > range.end }
-      end
-    start = size if start.nil?
-    finish = size + 1 if finish.nil?
+      return nil if start.nil?
+      return start, (size - 1) if finish.nil?
+      return nil if finish <= start
+    else
+      start = bsearch_index { |x| x >= target }
+      finish = bsearch_index { |x| x > target }
+      return nil if start.nil? || finish.nil? || finish <= start
+    end
     [start, finish - 1]
   end
 end
--- spec/bfind_spec.rb
+++ spec/bfind_spec.rb
@@ -11,4 +11,34 @@ RSpec.describe "Array#binary_find" do
     array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
     expect(array.binary_find(2..3)).to eq([2, 5])
   end
+
+  it "works with range whose start is over the last element" do
+    array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+    expect(array.binary_find(6..7)).to be_nil
+  end
+
+  it "works with range whose end is over the last element" do
+    array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+    expect(array.binary_find(4..6)).to eq([6, 9])
+  end
+
+  it "works with range which is between two elements" do
+    array = [1, 1, 2, 2, 3, 3, 6, 6, 9, 9]
+    expect(array.binary_find(4..5)).to be_nil
+  end
+
+  it "works with startless range" do
+    array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+    expect(array.binary_find(..3)).to eq([0, 5])
+  end
+
+  it "works with endless range" do
+    array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+    expect(array.binary_find(3..)).to eq([4, 9])
+  end
+
+  it "returns nil if not found" do
+    array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+    expect(array.binary_find(6)).to be_nil
+  end
 end
