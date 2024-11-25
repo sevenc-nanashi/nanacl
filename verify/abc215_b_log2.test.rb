140aeff40681515bf151114ed60507317f7835a2
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-11-25 19:49:59 +0900

    Delete: log2を消す

--- verify/abc215_b_log2.test.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-# verification-helper: PROBLEM https://atcoder.jp/contests/abc215/tasks/abc215_b
-
-require "nanacl/logi"
-in_n = gets.chomp.to_i
-
-puts Nanacl.logi(in_n, 2)
