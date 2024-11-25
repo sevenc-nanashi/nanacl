4a2dc61ff7e9d7f3991569e5de7ece1fe7563550
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-11-25 19:15:31 +0900

    Add: nanaclを直requireできるようにする

--- lib/nanacl.rb
+++ lib/nanacl.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
+require_relative "nanacl/array_bfind"
+require_relative "nanacl/array_sum"
+require_relative "nanacl/bsearch_right"
+require_relative "nanacl/const"
+require_relative "nanacl/dijkstra"
+require_relative "nanacl/logi"
+require_relative "nanacl/unionfind_map"
+require_relative "nanacl/unionfind_weight"
 require_relative "nanacl/version"
 
 module Nanacl
-  class Error < StandardError; end
+  class Error < StandardError
+  end
   # Your code goes here...
 end
